016fced5467f95c8c8b9bb6c9b07717afadad5cd
nadyeska <142436834+nadyeska@users.noreply.github.com>
2023-09-20 14:00:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+  
+ 
+  var name = "hadasa";
+  var sobrenome = "quintilhano";
+  var idade = "15";
+  var altura = "1.65";
+  var possuiCarteiraDeHabilitação = false;
+
+
+  console.log("oi, meu nome é "+ name+" "+ sobrenome+ "minha idade é "+ idade);
+
+
+  var conta1 = 10;
+  var conta2 = 20;
+
+  var resultado = conta1 + conta2;
+
+  console.log(resultado);
+
+  
+  var aluni = 14;
+  var maioridade = 18;
+
+  console.log("o aluno é maior de idade???");
+
+  if(aluni > maioridade) {
+    console.log('sim');
+  } else{
+      console.log('não');
+  }
+  
